e7e5a3bf21cdbf8207a32a64d15ae42e47516648
maxiamikel19 <maxloversist19@gmail.com>
2024-06-05 17:59:37 -0300

    Inicial commit

---
+++ database/migrations/2024_06_05_203918_create_produtos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->unsignedBigInteger('id');
+            $table->string('nome');
+            $table->string('descticao');
+            $table->double('preco');
+            $table->integer('estoque');
+            $table->unsignedBigInteger('categoria_id');
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
